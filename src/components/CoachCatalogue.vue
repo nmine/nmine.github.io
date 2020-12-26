c837e420843cb117ecb80a90ba2d76b741e9b79b
nicolasmiine <nicolas.mine11@gmail.com>
2020-12-26 09:58:42 +0100

    first commit

---
+++ src/components/CoachCatalogue.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container fluid class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col
+        :key="1"
+        cols="12"
+        lg="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          One of three columns
+        </v-card>
+      </v-col>
+      <v-col
+        :key="2"
+        cols="12"
+        lg="8"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          One of three columns
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'CoachCatalogue',
+
+    data: () => ({
+      show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+        states: [
+          'Alabama', 'Alaska', 'American Samoa', 'Arizona',
+          'Arkansas', 'California', 'Colorado', 'Connecticut',
+          'Delaware', 'District of Columbia', 'Federated States of Micronesia',
+          'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho',
+          'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+          'Louisiana', 'Maine', 'Marshall Islands', 'Maryland',
+          'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+          'Missouri', 'Montana', 'Nebraska', 'Nevada',
+          'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+          'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+          'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
+          'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+          'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
+          'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
+        ],
+    }),
+  }
+</script>
---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-form>
+    <v-container  fluid>
+      <v-card  class="mx-auto pa-md-8 mx-lg-auto"
+    max-width="500"
+  elevation="2">
+      <v-row >
+        <v-col
+          cols="12"
+          sm="12"
+        >
+
+        <v-text-field
+            :counter="50"
+            label="First name"
+            required
+          ></v-text-field>
+
+
+          <v-text-field
+            :counter="50"
+            label="Last name"
+            required
+          ></v-text-field>
+        <v-text-field
+          v-model="email"
+          label="E-mail"
+        ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+          <v-btn
+        class="mr-4"
+        color="primary"
+      >
+        Create Account
+      </v-btn>
+        </v-col>
+
+             </v-row>
+      </v-card>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'CreateAccount',
+
+    data: () => ({
+      show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+    }),
+  }
+</script>

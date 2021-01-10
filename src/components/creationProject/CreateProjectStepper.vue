95cda77219f6663a74623d53b52e0445a222390e
nicolasmiine <nicolas.mine11@gmail.com>
2021-01-10 11:35:56 +0100

    trainee creation/edition

--- src/components/creationProject/CreateProjectStepper.vue
+++ src/components/creationProject/CreateProjectStepper.vue
@@ -32,6 +32,10 @@
             <v-divider></v-divider>
 
             <v-stepper-step step="5" editable>Trainee</v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step step="6" editable>IDP</v-stepper-step>
         </v-stepper-header>
 
         <v-stepper-items>
@@ -392,6 +396,7 @@
                         :headers="headers"
                         :items="desserts"
                         sort-by="calories"
+                        group-by="category"
                         class="elevation-1"
                 >
                     <template v-slot:top>
@@ -399,6 +404,181 @@
                                 flat
                         >
                             <v-toolbar-title>Trainees</v-toolbar-title>
+                            <v-divider
+                                    class="mx-4"
+                                    inset
+                                    vertical
+                            ></v-divider>
+                            <v-spacer></v-spacer>
+                            <v-dialog
+                                    v-model="dialog"
+                                    max-width="500px"
+                            >
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn
+                                            color="primary"
+                                            dark
+                                            class="mb-2"
+                                            v-bind="attrs"
+                                            v-on="on"
+                                    >
+                                        New Item
+                                    </v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="headline">{{ formTitle }}</span>
+                                    </v-card-title>
+
+                                    <v-card-text>
+                                        <v-container>
+                                            <v-row>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-text-field
+                                                            v-model="editedItem.name"
+                                                            label="First Name Last Name"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-text-field
+                                                            v-model="editedItem.calories"
+                                                            label="Calories"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-text-field
+                                                            v-model="editedItem.fat"
+                                                            label="Fat (g)"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-text-field
+                                                            v-model="editedItem.carbs"
+                                                            label="Carbs (g)"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-text-field
+                                                            v-model="editedItem.protein"
+                                                            label="Protein (g)"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col
+                                                        cols="12"
+                                                        sm="6"
+                                                        md="4"
+                                                >
+                                                    <v-select
+                                                            v-model="value"
+                                                            :items="items"
+                                                            attach
+                                                            chips
+                                                            label="Specialities"
+                                                            multiple
+                                                    ></v-select>
+                                                </v-col>
+                                            </v-row>
+                                        </v-container>
+                                    </v-card-text>
+
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn
+                                                color="blue darken-1"
+                                                text
+                                                @click="close"
+                                        >
+                                            Cancel
+                                        </v-btn>
+                                        <v-btn
+                                                color="blue darken-1"
+                                                text
+                                                @click="save"
+                                        >
+                                            Save
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                            <v-dialog v-model="dialogDelete" max-width="500px">
+                                <v-card>
+                                    <v-card-title class="headline">Are you sure you want to delete this item?
+                                    </v-card-title>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                        <v-spacer></v-spacer>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-toolbar>
+                    </template>
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon
+                                small
+                                class="mr-2"
+                                @click="editItem(item)"
+                        >
+                            mdi-pencil
+                        </v-icon>
+                        <v-icon
+                                small
+                                @click="deleteItem(item)"
+                        >
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                        <v-btn
+                                color="primary"
+                                @click="initialize"
+                        >
+                            Reset
+                        </v-btn>
+                    </template>
+                </v-data-table>
+
+                <v-btn
+                        color="primary"
+                        @click="e1 = 1"
+                >
+                    Continue
+                </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="6">
+                <v-data-table
+                        :headers="headersIDP"
+                        :items="desserts"
+                        sort-by="calories"
+                        class="elevation-1"
+                >
+                    <template v-slot:top>
+                        <v-toolbar
+                                flat
+                        >
+                            <v-toolbar-title>IDP</v-toolbar-title>
                             <v-divider
                                     class="mx-4"
                                     inset
@@ -582,7 +762,15 @@
                 {text: 'Email', value: 'carbs'},
                 {text: 'Credit', value: 'protein'},
                 {text: 'Dates begin/end', value: 'dates'},
-                { text: 'Gluten-Free', value: 'sponsors' },
+                { text: 'Sponsor(s)', value: 'sponsors' },
+                { text: 'IDP', value: 'sponsors' },
+                {text: 'Actions', value: 'actions', sortable: false},
+            ],
+            headersIDP: [
+                {text: 'Headers', align: 'start', value: 'name'},
+                {text: 'Col1', value: 'calories'},
+                {text: 'Col2', value: 'fat'},
+                {text: 'Col3', value: 'carbs'},
                 {text: 'Actions', value: 'actions', sortable: false},
             ],
             desserts: [],
@@ -634,7 +822,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
-                        glutenfree: true,
+                        category: 'sponsor',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -644,7 +832,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
-                        glutenfree: true,
+                        category: 'sponsor',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -654,7 +842,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
-                        glutenfree: true,
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -664,6 +852,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -673,6 +862,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -682,6 +872,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -691,6 +882,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -700,6 +892,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -709,6 +902,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                     {
@@ -718,6 +912,7 @@
                         carbs: 'toto@gmail.com',
                         protein: 5,
                         sponsors: ["foo", "bar", "fizz", "buzz"],
+                        category: 'trainee',
                         dates: '05/01/21 - 05/03/21'
                     },
                 ]
